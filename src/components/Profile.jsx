4c5629179a61d9701dc1a41b1f2abfbb9f8316db
Greencyber-2 <166314729+Greencyber-2@users.noreply.github.com>
2025-07-19 22:57:10 +0330

    Update Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -71,7 +71,7 @@ const Profile = ({ onClose }) => {
 
         {/* Team Signature */}
         <div className="team-signature">
-          <span>با ❤️ از طرف جشنواره نوجونان خوارزمی</span>
+          <span>با ❤️ برای یازدهمین جشنواره نوجوانان خوارزمی</span>
           <small>نسخه ۱.۰.۰</small>
         </div>
       </div>
